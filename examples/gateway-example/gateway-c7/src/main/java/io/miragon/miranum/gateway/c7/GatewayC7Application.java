916664deb620c99b4c5b054a9442e70e6160d579
Julian Haupt <julian@haupt.media>
2023-03-08 00:07:55 +0100

    cloud events schema validation WIP

--- examples/gateway-example/gateway-c7/src/main/java/io/miragon/miranum/gateway/c7/GatewayC7Application.java
+++ examples/gateway-example/gateway-c7/src/main/java/io/miragon/miranum/gateway/c7/GatewayC7Application.java
@@ -17,16 +17,4 @@ public class GatewayC7Application {
     public static void main(final String[] args) {
         SpringApplication.run(GatewayC7Application.class, args);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            System.out.println("Registered Beans: ");
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-        };
-    }
 }
